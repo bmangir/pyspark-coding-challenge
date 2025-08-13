6b32d4ddff81f752d0926392dd34a761eb45df93
bmangir <berkantmangir23@gmail.com>
2025-08-13 20:18:58 +0300

    add data type checker and option to treat as single json doc

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,22 @@
-def read_json(spark, schema, path):
-    df = spark.read.schema(schema) \
+from pyspark.sql import SparkSession, DataFrame
+
+
+def read_json(spark: SparkSession, schema, path) -> DataFrame:
+    df = spark.read.schema(schema).option("multiLine", "true") \
         .json(path)
 
     return df
 
 
-def read_csv(spark, schema, path):
-    df = spark.read.schema(schema) \
+def read_csv(spark: SparkSession, schema, path) -> DataFrame:
+    df = spark.read.schema(schema).option("multiLine", "true") \
         .csv(path)
 
     return df
 
 
-def read_parquet(spark, schema, path):
-    df = spark.read.schema(schema) \
+def read_parquet(spark: SparkSession, schema, path) -> DataFrame:
+    df = spark.read.schema(schema).option("multiLine", "true") \
         .parquet(path)
 
     return df
