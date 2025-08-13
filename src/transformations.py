dc2cd70570a2791c4e0494ba6b6831dfae1c5809
bmangir <berkantmangir23@gmail.com>
2025-08-13 20:22:39 +0300

    preprocess the data and make func private

--- src/transformations.py
+++ src/transformations.py
@@ -13,20 +13,20 @@ class Transformer:
     def __repr__(self):
         return f"Transformer(name={self.name})"
 
-    def prepare_impressions(self, df):
+    def __prepare_impressions__(self, df):
         return df
 
-    def prepare_clicks(self, df):
+    def __prepare_clicks__(self, df):
         return df.withColumn("action_type", F.lit(ACTION_CLICK)) \
             .withColumnRenamed("item_id", "action_item_id") \
             .withColumnRenamed("click_time", "action_time")
 
-    def prepare_add_to_carts(self, df):
+    def __prepare_add_to_carts__(self, df):
         return df.withColumn("action_type", F.lit(ACTION_ATC)) \
             .withColumnRenamed("config_id", "action_item_id") \
             .withColumnRenamed("occurred_at", "action_time")
 
-    def prepare_previous_orders(self, df):
+    def __prepare_previous_orders__(self, df):
         return df.withColumn("action_type", F.lit(ACTION_ORD)) \
             .withColumnRenamed("config_id", "action_item_id") \
             .withColumnRenamed("order_date", "action_time")
@@ -36,7 +36,10 @@ class Transformer:
         :param args: impressions, clicks, add_to_carts, previous_orders dfs respectively
         :return:
         """
-        pass
+        return (self.__prepare_impressions__(args[0]),
+                self.__prepare_clicks__(args[1]),
+                self.__prepare_add_to_carts__(args[2]),
+                self.__prepare_previous_orders__(args[3]))
 
     def tag(self):
         pass
