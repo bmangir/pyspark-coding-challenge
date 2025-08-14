ac188b255c752480db65348332cffd101b7294b2
bmangir <berkantmangir23@gmail.com>
2025-08-14 20:09:53 +0300

    filter data before union then union

--- src/transformations.py
+++ src/transformations.py
@@ -1,11 +1,15 @@
 from pyspark.sql import functions as F, Window
+import datetime
 
 from src.config import ACTION_CLICK, ACTION_ATC, ACTION_ORD
 
 
 class Transformer:
-    def __init__(self, name: str):
+    def __init__(self, name: str, n_actions=1000, days=365):
         self.name = name
+        self.n_actions = n_actions
+        self.days = days
+        self.reference_date = None
 
     def transform(self, data):
         raise NotImplementedError("This method should be overridden by subclasses")
@@ -14,22 +18,40 @@ class Transformer:
         return f"Transformer(name={self.name})"
 
     def __prepare_impressions__(self, df):
+        impression_dates = df.select(F.max("dt").alias("max_dt")).collect()[0]["max_dt"]
+
+        self.reference_date = F.date_sub(F.lit(impression_dates), self.days)
         return df
 
     def __prepare_clicks__(self, df):
-        return df.withColumn("action_type", F.lit(ACTION_CLICK)) \
+        renamed_df = df.withColumn("action_type", F.lit(ACTION_CLICK)) \
             .withColumnRenamed("item_id", "action_item_id") \
-            .withColumnRenamed("click_time", "action_time")
+            .withColumnRenamed("click_time", "action_time") \
+            .select("customer_id", "action_item_id", "action_time", "action_type")
+
+        filtered_df = renamed_df.filter(F.col("action_time") >= self.reference_date)
+
+        return filtered_df
 
     def __prepare_add_to_carts__(self, df):
-        return df.withColumn("action_type", F.lit(ACTION_ATC)) \
+        renamed_df = df.withColumn("action_type", F.lit(ACTION_ATC)) \
             .withColumnRenamed("config_id", "action_item_id") \
-            .withColumnRenamed("occurred_at", "action_time")
+            .withColumnRenamed("occurred_at", "action_time") \
+            .select("customer_id", "action_item_id", "action_time", "action_type")
+
+        filtered_df = renamed_df.filter(F.col("action_time") >= self.reference_date)
+
+        return filtered_df
 
     def __prepare_previous_orders__(self, df):
-        return df.withColumn("action_type", F.lit(ACTION_ORD)) \
+        renamed_df = df.withColumn("action_type", F.lit(ACTION_ORD)) \
             .withColumnRenamed("config_id", "action_item_id") \
-            .withColumnRenamed("order_date", "action_time")
+            .withColumnRenamed("order_date", "action_time") \
+            .select("customer_id", "action_item_id", "action_time", "action_type")
+
+        filtered_df = renamed_df.filter(F.col("action_time") >= F.date_sub(F.current_date(), self.days)) \
+
+        return filtered_df
 
     def prepare_data(self, *args):
         """
@@ -44,8 +66,8 @@ class Transformer:
     def tag(self):
         pass
 
-    def build_action_history(self):
-        pass
+    def build_action_history(self, clicks_df, carts_df, orders_df):
+        return clicks_df.unionByName(carts_df).unionByName(orders_df)
 
     def join_impressions_with_actions(self):
         pass
